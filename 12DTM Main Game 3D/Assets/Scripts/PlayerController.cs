3c91b2bdebf73066ad04af95d8f9e36b6e561fa0
Dave-Ashby <19004@wc.school.nz>
2022-08-16 14:21:20 +1200

    Commit 7
    Made the crouching affect how the player moves.

--- 12DTM Main Game 3D/Assets/Scripts/PlayerController.cs
+++ 12DTM Main Game 3D/Assets/Scripts/PlayerController.cs
@@ -28,7 +28,16 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime  * horizontalSpeed, Space.World);
 
-        if (Input.GetKeyDown(KeyCode.W) && isTouchingGround == true)
+        if (horizontalInput > 0)
+        {
+            gameObject.transform.localScale = new Vector3(25, 25, 25);
+        }
+        if (horizontalInput < 0)
+        {
+            gameObject.transform.localScale = new Vector3(-25, 25, 25);
+        }
+
+            if (Input.GetKeyDown(KeyCode.W) && isTouchingGround == true)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
@@ -42,5 +51,14 @@ public class PlayerController : MonoBehaviour
     {
         isTouchingGround = false;
     }
-
+    public void Crouching()
+    {
+        horizontalSpeed = 2.0f;
+        jumpForce = 3.0f;
+    }
+    public void NotCrouching()
+    {
+        horizontalSpeed = 5.0f;
+        jumpForce = 5.0f;
+    }
 }
--- 12DTM Main Game 3D/Assets/Scripts/PlayerCrouch.cs
+++ 12DTM Main Game 3D/Assets/Scripts/PlayerCrouch.cs
@@ -18,17 +18,15 @@ public class PlayerCrouch : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S) && isCrouching == false)
         {
- 
             transform.Translate(Vector3.down * crouchForce);
             isCrouching = true;
-           
+            GameObject.Find("Player").GetComponent<PlayerController>().Crouching();
         }
         if (Input.GetKeyUp(KeyCode.S))
         {
-
             transform.Translate(Vector3.up * crouchForce);
             isCrouching = false;
-
+            GameObject.Find("Player").GetComponent<PlayerController>().NotCrouching();
         }
     }
 
