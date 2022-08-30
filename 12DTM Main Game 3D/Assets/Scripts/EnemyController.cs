43d7cbc5eed0c71eb6c48477f5c495c8ee2e9eab
Dave-Ashby <19004@wc.school.nz>
2022-08-30 14:18:55 +1200

    Commit 10
    worked on enemy AI

--- 12DTM Main Game 3D/Assets/Scripts/EnemyController.cs
+++ 12DTM Main Game 3D/Assets/Scripts/EnemyController.cs
@@ -9,8 +9,12 @@ public class EnemyController : MonoBehaviour
 
     public Transform player;
 
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
     public Vector3 walkPoint;
     public float walkPointRange;
+    public bool walkPointSet;
 
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
@@ -23,11 +27,26 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        playerInSightRange = Physics.CheckSphere
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
     }
+
     void Patrolling()
     {
+        if (!walkPointSet) SearchWalkPoint();
+        if (walkPointSet) agent.SetDestination(walkPoint);
+    }
 
+    void SearchWalkPoint()
+    {
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
     }
 
     void ChasePlayer()
