231a08fbfe595cfc47f7b7a3844595a577d203b0
Dave-Ashby <19004@wc.school.nz>
2022-10-18 14:19:34 +1300

    Commit 13
    Coded Camera

--- 12DTM Main Game 3D/Assets/Scripts/CameraController.cs
+++ 12DTM Main Game 3D/Assets/Scripts/CameraController.cs
@@ -14,12 +14,15 @@ class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        xOffset = 2f;
+        yOffset = 2.5f;
+        zOffset = -9f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(player.position.x + xOffset, player.position.y + yOffset, player.position.z + zOffset);
     }
 }
